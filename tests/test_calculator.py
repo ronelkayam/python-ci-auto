c3a5f157aaed8908508dd58c55eb47d155cad3d9
ron elkayam <ronelk6@gmail.com>
2026-01-19 22:03:58 +0200

    failed test CI

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,17 +1,26 @@
 import pytest
 from services.calculator_service import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(5, 3) == 2
 
+
 def test_multiply():
     assert multiply(4, 3) == 12
 
+
+def test_failed():
+    assert divide(1, 0)
+
+
 def test_divide():
     assert divide(10, 2) == 5
+
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
