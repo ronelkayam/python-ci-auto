3184e14b613b5a17ea48d461f99fc09d9f687c70
jonathansh44 <yoni123456747@gmail.com>
2026-01-19 20:48:37 +0200

    mistake

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,7 +8,7 @@ def test_subtract():
     assert subtract(5, 3) == 2
 
 def test_multiply():
-    assert multiply(4, 2) == 2
+    assert multiply(4, 2) == 4
 
 def test_divide():
     assert divide(10, 2) == 5
