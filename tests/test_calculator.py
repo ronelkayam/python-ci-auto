5df54336644af03dec666c39ec94f217a6acf70f
jonathansh44 <yoni123456747@gmail.com>
2026-01-19 19:40:12 +0200

    wrong multiply

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,7 +8,7 @@ def test_subtract():
     assert subtract(5, 3) == 2
 
 def test_multiply():
-    assert multiply(4, 2) == 4
+    assert multiply(4, 2) == 3
 
 def test_divide():
     assert divide(10, 2) == 5
