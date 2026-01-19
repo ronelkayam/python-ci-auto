e7b93776046737e7b832cdaa8930fdcf95301729
ron elkayam <ronelk6@gmail.com>
2026-01-19 19:23:59 +0200

    support

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculator import add, subtract, multiply, divide
+from services.calculator_service import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
