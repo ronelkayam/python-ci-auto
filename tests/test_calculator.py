43bffaaeace0f4bc9776924d7a48e799f9619409
ronelkayam <68506578+ronelkayam@users.noreply.github.com>
2026-01-21 20:54:57 +0300

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,7 +8,7 @@ def test_subtract():
     assert subtract(5, 3) == 2
 
 def test_multiply():
-    assert multiply(4, 3) == 15
+    assert multiply(4, 3) == 12
 
 def test_divide():
     assert divide(10, 2) == 5
